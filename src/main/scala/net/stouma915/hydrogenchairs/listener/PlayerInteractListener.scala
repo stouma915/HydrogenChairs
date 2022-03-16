4110f08b305f698240b9b67ba94d74e477229346
stouma915 <sasakitouma@yahoo.ne.jp>
2022-03-17 05:21:24 +0900

    椅子から離れすぎている時エラーを出す

--- src/main/scala/net/stouma915/hydrogenchairs/listener/PlayerInteractListener.scala
+++ src/main/scala/net/stouma915/hydrogenchairs/listener/PlayerInteractListener.scala
@@ -3,6 +3,7 @@ package net.stouma915.hydrogenchairs.listener
 import cats.effect.IO
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.TextColor
+import net.stouma915.hydrogenchairs.implicits.*
 import org.bukkit.block.data.`type`.Stairs
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.{EventHandler, Listener}
@@ -14,6 +15,14 @@ class PlayerInteractListener extends Listener {
 
   @EventHandler
   def onPlayerInteract(event: PlayerInteractEvent): Unit = {
+    val nullChecks = Set(
+      event.getHand.nonNull,
+      event.getClickedBlock.nonNull,
+      event.getPlayer.nonNull
+    )
+    if (!nullChecks.forall(x => x))
+      return ()
+
     if (event.getHand != EquipmentSlot.HAND)
       return ()
     if (!event.getClickedBlock.getBlockData.isInstanceOf[Stairs])
@@ -25,6 +34,30 @@ class PlayerInteractListener extends Listener {
           event.getPlayer.sendActionBar(
             Component
               .text("You do not have permission.")
+              .color(TextColor.color(255, 85, 85))
+          )
+        }
+      } yield ()
+
+      return program.unsafeRunSync()
+    }
+
+    val stairsLocation = event.getClickedBlock.getLocation
+    val playerLocation = event.getPlayer.getLocation
+
+    val differenceX =
+      math.abs(stairsLocation.getBlockX - playerLocation.getBlockX)
+    val differenceY =
+      math.abs(stairsLocation.getBlockY - playerLocation.getBlockY)
+    val differenceZ =
+      math.abs(stairsLocation.getBlockZ - playerLocation.getBlockZ)
+
+    if ((differenceX + differenceZ) >= 3 || differenceY >= 3) {
+      val program = for {
+        _ <- IO {
+          event.getPlayer.sendActionBar(
+            Component
+              .text("Chair is too far.")
               .color(TextColor.color(255, 85, 85))
           )
         }
